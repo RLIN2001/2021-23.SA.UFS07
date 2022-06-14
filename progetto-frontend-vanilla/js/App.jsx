4d1abcfc45ce57d64366831267f51ccfac73d735
Riccardo <chenweiriccardo.lin@gmail.com>
2022-06-14 12:54:44 +0200

    feat: setup version and add input

--- progetto-frontend-vanilla/js/App.jsx
+++ progetto-frontend-vanilla/js/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import {Button} from "test-1-riccardo/atoms/button/Button"
+import {Input} from "test-1-riccardo/atoms/input/Input"
+
 
 const App = () => ( 
     <div>
         <h1>Hello React</h1>
         <Button label={"gasssss"} />
+        <Input id={"20"}></Input>
     </div>
 );
 
