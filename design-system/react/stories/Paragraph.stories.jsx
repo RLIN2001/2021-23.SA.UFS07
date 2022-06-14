8ea9c4bf51be434114046bcbd7ac201abc07f78b
Riccardo <chenweiriccardo.lin@gmail.com>
2022-06-14 12:55:47 +0200

    fix: add paragraph component

---
+++ design-system/react/stories/Paragraph.stories.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text } from '../src/atoms/text/Text';
+import { Paragraph} from '../src/molecules/paragraph/Paragraph';
+
+export default {
+    title: 'Example/Molecules/Paragraph',
+    component: Paragraph,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+    },
+  };
+
+  const Template = (args) => <Paragraph {...args} />;
+
+  export const Par = Template.bind({});
+Par.args = {
+   children:<Text label={'label'}></Text>
+};
